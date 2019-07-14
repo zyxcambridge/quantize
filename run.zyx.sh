055eeb9b0d49ae77f88a58df0c44bee89842ce3f
Qiuchi Li <qiuchili@dei.unipd.it>
2019-07-14 18:57:10 +0800

    add the how to use mutil gpu train run.zyx.sh

--- run.zyx.sh
+++ run.zyx.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+#mutil gpu train 
+# vim scripts/run_local.sh  nb_gpus=8
+./scripts/run_local.sh main.py     --learner full-prec
 # docker run -it -v $PWD:/PocketFlow registry.cn-shanghai.aliyuncs.com/snowlake/tensorflow:quantize_v2 bash
 
 
